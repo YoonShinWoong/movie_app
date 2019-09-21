c4ea06d0597a545617a6b98714e13550f30ea80b
YoonShinWoong <tlsdnd7897@likelion.org>
2019-09-21 23:10:43 +0900

    Updates

---
+++ precache-manifest.ad0dbe2dc9aae384651d9b84ccc68891.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4296d196b3c172650cef0c4e2177ee03",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "43b5bc532f7e47010428",
+    "url": "/movie_app/static/css/main.731b0bf4.chunk.css"
+  },
+  {
+    "revision": "be7700fc3c2d5a16e87a",
+    "url": "/movie_app/static/js/2.1d967ade.chunk.js"
+  },
+  {
+    "revision": "43b5bc532f7e47010428",
+    "url": "/movie_app/static/js/main.08c02a58.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
